eb68998cc617b519a24487b565ed58797856dee2
Samuel Islas Mateos <samuelislas@outlook.com>
2023-12-05 14:13:58 -0500

    Desing of thumbnail

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThumbnailComponent } from './thumbnail/thumbnail.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 
@@ -9,9 +10,24 @@ import { ThumbnailComponent } from './thumbnail/thumbnail.component';
     ThumbnailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgCircleProgressModule.forRoot({
+      backgroundColor: "#000",
+      titleFontSize: "12",
+      radius: 20,
+      maxPercent: 100,
+      units: "%",
+      outerStrokeWidth: 5,
+      showSubtitle: false,
+      showInnerStroke: false,
+      startFromZero: false,
+      innerStrokeColor: "#FFFFFF",
+      titleColor: "#FFFFFF",
+      unitsColor: "#FFFFFF",
+      titleFontWeight: "500",
+    })
   ],
-  exports:[
+  exports: [
     ThumbnailComponent
   ]
 })
