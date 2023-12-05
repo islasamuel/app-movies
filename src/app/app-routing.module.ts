fd8e5d98803d0f7e0667bb9304cd62875c27ed77
Samuel Islas Mateos <samuelislas@outlook.com>
2023-12-05 10:38:33 -0500

    feature: add base component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesComponent } from './pages/movies/movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies', component: MoviesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true }),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
